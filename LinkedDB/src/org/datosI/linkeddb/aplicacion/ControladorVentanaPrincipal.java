679504dcac040d79caa7a943448505a7925862ce
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2017-09-20 22:57:31 -0600

    Establecimiento de la base para la interfaz gráfica

--- LinkedDB/src/org/datosI/linkeddb/aplicacion/ControladorVentanaPrincipal.java
+++ LinkedDB/src/org/datosI/linkeddb/aplicacion/ControladorVentanaPrincipal.java
@@ -1,5 +1,13 @@
 package org.datosI.linkeddb.aplicacion;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class ControladorVentanaPrincipal {
+	
+    @FXML
+    private ImageView Imagen_Aplicacion;
+    
+
 
 }
--- LinkedDB/src/org/datosI/linkeddb/aplicacion/Main.java
+++ LinkedDB/src/org/datosI/linkeddb/aplicacion/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -13,6 +15,8 @@ public class Main extends Application{
 		Parent root = FXMLLoader.load(getClass().getResource("/org/datosI/linkeddb/aplicacion/VentanaPrincipal.fxml"));
 		Scene scene = new Scene(root);
 		
+
+		primaryStage.setTitle("LinkedDB");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
